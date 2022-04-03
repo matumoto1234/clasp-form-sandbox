eaa9e038ca36f309ec2a8090456cf311f498f29e
matumoto1234 <matumotosizuoka@gmail.com>
2022-04-03 17:38:03 +0900

    Implement onSubmit

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
-const test = () => {
-  console.log("hello world");
-};
+const properties = PropertiesService.getScriptProperties().getProperties();
+const WEBHOOK_URL = properties.WEBHOOK_URL;
+
+const onSubmit = (e: any) => {
+  const data = (e.response as GoogleAppsScript.Forms.FormResponse)
+    .getItemResponses()[0]
+    ?.getResponse()
+
+  const body: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+    method: 'post',
+    contentType: 'application/json',
+    payload: JSON.stringify({
+      content: data
+    })
+  }
+
+  UrlFetchApp.fetch(WEBHOOK_URL, body)
+}
