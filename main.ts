12a97dbd3783ba71224722e5e0ca1040b0537d25
matumoto1234 <matumotosizuoka@gmail.com>
2022-04-05 03:06:11 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,67 @@
 const properties = PropertiesService.getScriptProperties().getProperties();
 const WEBHOOK_URL = properties.WEBHOOK_URL;
 
+
+type DataType = string | string[] | string[][];
+
+const dataToString = (s: DataType): string => {
+  switch (typeof s) {
+    case 'string':
+      return s;
+    case 'object':
+      switch (typeof s[0]) {
+        case 'string':
+          return s.join('\n');
+        case 'object':
+          // 二次元配列を一次元にしてそれを'\n'区切りのstringに
+          return [].concat(s).join('\n');
+        default:
+          return '';
+      }
+    default:
+      return '';
+  }
+}
+
+const escapeBackquotes = (s: string): string => {
+  let result = '';
+  for (const char of s) {
+    if (char === '`') {
+      result = result + '\\`';
+      continue;
+    }
+    result = result + char;
+  }
+  return result;
+};
+
 const onSubmit = (e: any) => {
-  const data = (e.response as GoogleAppsScript.Forms.FormResponse)
-    .getItemResponses()[0]
-    ?.getResponse()
+  const response: GoogleAppsScript.Forms.FormResponse = e.response;
+  const itemResponses: GoogleAppsScript.Forms.ItemResponse[] = response.getItemResponses();
+
+  let dataList: DataType[] = [];
+
+  for (const itemResponse of itemResponses) {
+    const title: string = itemResponse.getItem().getTitle();
+    const helpText: string = itemResponse.getItem().getHelpText();
+    const res: DataType = itemResponse.getResponse();
+
+    let data = `\`\`\`【${title}】\n`;
+    if (helpText !== '') {
+      data = data + `${helpText}\n`;
+    }
+    const strRes = dataToString(res);
+    const escaped = escapeBackquotes(strRes);
+    data = data + `${escaped}\n\`\`\``;
+
+    dataList.push(data)
+  }
 
   const body: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
     method: 'post',
     contentType: 'application/json',
     payload: JSON.stringify({
-      content: data
+      content: dataList.join('\n')
     })
   }
 
